b430efb4d7ca326741eba10e4b85123d66b46334
Ryan Steffan <ryansteffan.biz@gmail.com>
2025-02-04 15:01:35 -0600

    Added 404 page to the website

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,3 +1,13 @@
-export default function NotFound() {
-  return <div>404 - Page Not Found</div>;
+export default function NotFound(): React.ReactNode {
+  return (
+    <>
+      <div className="m-4 w-fit p-4">
+        <span className="text-3xl font-extrabold text-teal-300">Oh No!</span>
+        <br />
+        <span className="text-lg font-bold">
+          Error 404 - The page could not be found.
+        </span>
+      </div>
+    </>
+  );
 }
